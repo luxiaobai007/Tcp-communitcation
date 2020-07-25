37c809570c4d1432f5c049d5718deea6c1e6a11b
lushengyang <1575018859@qq.com>
2020-07-25 21:30:32 +0800

    创建项目
    Signed-off-by: lushengyang <1575018859@qq.com>

---
+++ src/main/java/com/mina/TcpCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.mina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcpCommunicationApplication.class, args);
+	}
+
+}
